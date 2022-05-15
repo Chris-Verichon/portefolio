4c0b1a2f8d703fa54a5b579cecd36a0f3b1af9f0
Chris-Verichon <chris.verichon@gmail.com>
2022-05-15 14:54:49 +0200

    First commit start project

---
+++ src/components/App/index.js
@@ -0,0 +1,16 @@
+// == Import
+import reactLogo from './react-logo.svg';
+import './styles.css';
+
+// == Composant
+function App() {
+  return (
+    <div className="app">
+      <img src={reactLogo} alt="react logo" />
+      <h1>Portefolio Christophe Verichon</h1>
+    </div>
+  );
+}
+
+// == Export
+export default App;
