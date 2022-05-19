534ca28539a295a35099a849c5c62e23a3b4a239
Chris-Verichon <chris.verichon@gmail.com>
2022-05-19 11:55:47 +0200

    responsive home and nav

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,12 +2,14 @@
 import Bio from '../Bio';
 import Competences from '../Competences';
 import Header from '../Header';
+import Nav from '../Nav';
 import './styles.scss';
 
 // == Composant
 function App() {
   return (
     <div className="app">
+      <Nav />
       <Header />
       <Bio />
       <Competences />
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,22 +1,15 @@
 import './style.scss';
-import accueil from 'src/assets/images/accueil.gif';
 
 function Header() {
   return (
     <header id="home" className="header">
-      <nav className="header_navigation">
-        <a className="header_navigation-item" href="#bio"> Bio</a>
-        <a className="header_navigation-item" href="#competences"> Compétences</a>
-        <a className="header_navigation-item" href="/"> Parcours Pro</a>
-        <a className="header_navigation-item" href="/"> Réalisations</a>
-        <a className="header_navigation-item" href="/"> Contact</a>
-      </nav>
-      <a className="header_home" href="/"> <img className="header_home-img" src={accueil} alt="logo accueil" /></a>
-      <div className="header_title">
-        <h1 className="header_title-item">Développeur Web FullStack</h1>
-      </div>
-      <div className="header_title">
-        <h2 className="header_title-subtitle">Christophe Verichon</h2>
+      <div className="header-container">
+        <div className="header_title">
+          <h1 className="header_title-item">Développeur Web FullStack</h1>
+        </div>
+        <div className="header_subtitle">
+          <h2 className="header_subtitle-subtitle">Christophe Verichon</h2>
+        </div>
       </div>
     </header>
   );
