78bf97c34f99477223927d33255fc5d63ceac3c7
Chris-Verichon <chris.verichon@gmail.com>
2022-07-04 21:26:41 +0200

    skill page being static

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -4,7 +4,8 @@ import { useSelector } from 'react-redux';
 
 const Skills = () => {
   const { myData } = useSelector((state) => state.personnal);
-  const { skillsFront, skillsBack } = myData[0];
+  const { skillsFront, skillsBack, bdd } = myData[0];
+
   return (
     <div id="competences" className="competences">
       <div className="competences_content">
@@ -23,7 +24,7 @@ const Skills = () => {
           j'aime le contacte humain et m'adapte très facilement à mes paires.
         </p>
 
-        <h2 className="competences_transverses">Compétences transverses :</h2>
+        <h2 className="competences_transverses">Compétences transverses</h2>
         <p className="competences_transverses-content">
           Fort de mon expérience passé en tant que
           Responsable de Magasin dans plusieurs enseignes de
@@ -35,28 +36,69 @@ const Skills = () => {
           Ces expériences mon également appris la relation client, à être à l'écoute du client,
           de bien saisir ces besoins et d'y répondre.
         </p>
-        <h2 className="competences_list-title">Skills Front-end :</h2>
-        <ul className="competences_content-list">
-          {skillsFront.map((item) => (
-            <li
-              key={item.id}
-              className="competences_content-list--item"
-            >
-              {item.skillName}
-            </li>
-          ))}
-        </ul>
-        <h2 className="competences_list-title"> Skills Back-end :</h2>
-        <ul className="competences_content-list">
-          {skillsBack.map((item) => (
-            <li
-              key={item.id}
-              className="competences_content-list--item"
-            >
-              {item.skillName}
-            </li>
-          ))}
-        </ul>
+        <span className="separator" />
+        <h2 className="competences_techniques">Compétences Techniques</h2>
+        <section>
+          <h3 className="competences_list-title">Compétences Front-end :</h3>
+          <ul className="competences_content-list">
+            {skillsFront.map((item) => (
+              <li
+                key={item.id}
+                className="competences_content-list--item"
+              >
+                {item.logo === '' ? <span className="span">{item.skillName}</span> : <img src={item.logo} alt="logo" />}
+              </li>
+            ))}
+          </ul>
+        </section>
+        <section>
+          <h3 className="competences_list-title"> Compétences Back-end :</h3>
+          <ul className="competences_content-list">
+            {skillsBack.map((item) => (
+              <li
+                key={item.id}
+                className="competences_content-list--item"
+              >
+                {item.logo === '' ? <span className="span">{item.skillName}</span> : <img src={item.logo} alt="logo" />}
+              </li>
+            ))}
+          </ul>
+          <p className="competence_section-content">
+            Développeur fullstack je suis capable de développé une application
+            front avec html & css,
+            js vanilla, utiliser des framework front tel que bootstrap, ou encore Tailwin.
+          </p>
+          <p className="competence_section-content">
+            Et également en utilisant React en "single page application" avec l'utilisation
+            de Redux pour gérer le state et Axios pour consommer une API
+          </p>
+          <p className="competence_section-content">
+            Mais je suis également capable de développé tout le coté serveur (back-end)
+            avec php soit en créant une API Rest ou encore en créant un MVC (Model view controler).
+            Des connaissances également en Symfony et Laravel et initiation à NodeJs avec Express.
+            Je suis capable de m'adapter, j'aime apprendre et découvrir.
+          </p>
+        </section>
+        <section>
+          <h3 className="competences_list-title"> Administration de bases de données :</h3>
+          <ul className="competences_content-list">
+            {bdd.map((item) => (
+              <li
+                key={item.id}
+                className="competences_content-list--item"
+              >
+                {item.logo === '' ? <span className="span">{item.skillName}</span> : <img src={item.logo} alt="logo" />}
+              </li>
+            ))}
+          </ul>
+          <p>
+            Pour les bases de données relationnelles,
+            j'ai appris à travailler avec MySQL/MariaDB.
+          </p>
+          <p>
+            De la modélisation à la réalisation de la base de données, rédiger des requêtes custom.
+          </p>
+        </section>
       </div>
       <div>
         annimation à venir !!
--- src/data.js
+++ src/data.js
@@ -1,3 +1,12 @@
+import react from 'src/assets/images/atom.png';
+import htmlBig from 'src/assets/images/html-5big.png';
+import cssBig from 'src/assets/images/cssBig.png';
+import jsBig from 'src/assets/images/jsBig.png';
+import sassBig from 'src/assets/images/sassBig.png';
+import typescriptBig from 'src/assets/images/typescriptBig.png';
+import phpBig from 'src/assets/images/phpBig.png';
+import nodejsBig from 'src/assets/images/nodejsbig.png';
+
 const data = [
   {
     firstName: 'Verichon',
@@ -9,32 +18,32 @@ const data = [
       {
         id: 1,
         skillName: 'HTML5',
-        logo: '',
+        logo: htmlBig,
       },
       {
         id: 2,
         skillName: 'CSS3',
-        logo: '',
+        logo: cssBig,
       },
       {
         id: 3,
-        skillName: 'SCSS',
-        logo: '',
+        skillName: 'SASS',
+        logo: sassBig,
       },
       {
         id: 4,
         skillName: 'JavaScript',
-        logo: '',
+        logo: jsBig,
       },
       {
         id: 5,
         skillName: 'TypeScript',
-        logo: '',
+        logo: typescriptBig,
       },
       {
         id: 6,
         skillName: 'REACT',
-        logo: '',
+        logo: react,
       },
       {
         id: 7,
@@ -47,11 +56,11 @@ const data = [
         logo: '',
       },
     ],
-    skillsBscssack: [
+    skillsBack: [
       {
         id: 1,
         skillName: 'PHP',
-        logo: '',
+        logo: phpBig,
       },
       {
         id: 2,
@@ -61,7 +70,12 @@ const data = [
       {
         id: 3,
         skillName: 'NodeJs',
-        logo: '',
+        logo: nodejsBig,
+      },
+      {
+        id: 4,
+        skillName: 'JavaScript',
+        logo: jsBig,
       },
     ],
     bdd: [
@@ -73,6 +87,11 @@ const data = [
       {
         id: 2,
         skillName: 'SQL',
+        logo: '',
+      },
+      {
+        id: 3,
+        skillName: 'MySQL',
         logo: '',
       },
     ],
