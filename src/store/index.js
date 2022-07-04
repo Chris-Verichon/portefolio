2f1595bc1b09601922e9bebe6a7f2a8fbc619ec4
Chris-Verichon <chris.verichon@gmail.com>
2022-07-04 10:54:13 +0200

    create store, and reducer for redux

---
+++ src/store/index.js
@@ -0,0 +1,14 @@
+import { legacy_createStore as createStore } from 'redux';
+
+import { devToolsEnhancer } from 'redux-devtools-extension';
+
+import reducer from 'src/reducers';
+
+const store = createStore(
+  // reducer
+  reducer,
+  // enhancer
+  devToolsEnhancer(),
+);
+
+export default store;
