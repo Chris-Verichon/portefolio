055e0fd00f1db794a844aa9745921ef195a184e6
Chris-Verichon <chris.verichon@gmail.com>
2022-07-12 17:58:38 +0200

    page project done

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -9,6 +9,7 @@ import HomeCanvas from '../HomeCanvas';
 // Import components
 import HomeMobile from '../HomeMobile';
 import Error404 from '../Error/Error404';
+import Project from '../Project';
 
 // == Import style
 import './styles.scss';
@@ -20,6 +21,7 @@ function App() {
         <Route path="/" element={<><Nav /><HomeCanvas /><HomeMobile /></>} />
         <Route path="/A-propos" element={<><Nav /><Bio /></>} />
         <Route path="/Skills" element={<><Nav /><Skills /></>} />
+        <Route path="/Projets" element={<><Nav /><Project /></>} />
         <Route path="*" element={<Error404 />} />
       </Routes>
       {/* <HomeMobile /> */}
